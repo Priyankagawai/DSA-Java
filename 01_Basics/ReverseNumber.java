00fc8564953849270e99f63c8979a364eb27b8ed
Priyanka Gawai <priyankagawai092@gmail.com>
2026-01-03 18:55:15 +0530

    Solved reverse number problem in Java

---
+++ 01_Basics/ReverseNumber.java
@@ -0,0 +1,18 @@
+// Problem: Reverse a given number
+// Input: 1234
+// Output: 4321
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+        int num = 1234;
+        int rev = 0;
+
+        while (num != 0) {
+            int digit = num % 10;
+            rev = rev * 10 + digit;
+            num /= 10;
+        }
+
+        System.out.println(rev);
+    }
+}
