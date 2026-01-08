9e3488365a1e33ea42379943409b595960f7ad00
Priyanka Gawai <priyankagawai092@gmail.com>
2026-01-08 23:46:58 +0530

    Added Fibonacci Series program in Java

---
+++ 01_Basics/FibonacciSeries.java
@@ -0,0 +1,20 @@
+// Problem: Print Fibonacci series up to n terms
+// Input: n = 10
+// Output: 0 1 1 2 3 5 8 13 21 34
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        int n = 10;   // number of terms
+        int first = 0, second = 1;
+
+        System.out.print("Fibonacci Series: ");
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(first + " ");
+
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+    }
+}
