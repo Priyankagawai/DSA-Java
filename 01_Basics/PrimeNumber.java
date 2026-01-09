f679b3d8d8c0ac82a5172016b34764634b450585
Priyanka Gawai <priyankagawai092@gmail.com>
2026-01-09 15:52:04 +0530

    Solved Check Prime Number program in Java

---
+++ 01_Basics/PrimeNumber.java
@@ -0,0 +1,26 @@
+// Problem: Check whether a number is Prime
+// Input: 17
+// Output: Prime number
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        int num = 17;
+        boolean isPrime = true;
+
+        if (num <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i <= num / 2; i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+        if (isPrime) {
+            System.out.println("Prime number");
+        } else {
+            System.out.println("Not a prime number");
+        }
+    }
+}
